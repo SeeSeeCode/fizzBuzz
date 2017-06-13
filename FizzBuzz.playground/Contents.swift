f14d38fdb5e4973e4086cf1233ca00f05237e065
Kevin Thompson <kevinthompson86@gmail.com>
2017-06-12 18:29:35 -0700

    Add files via upload

---
+++ FizzBuzz.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func FizzBuzz() {
+    for i in 1...100 {
+        switch i {
+        case _ where i % 3 == 0 && i % 5 == 0:
+            print("FizzBuzz")
+        case _ where i % 3 == 0:
+            print("Fizz")
+        case _ where i % 5 == 0:
+            print("Buzz")
+        default:
+            print("\(i)")
+        }
+    }
+}
+
+FizzBuzz()
+
